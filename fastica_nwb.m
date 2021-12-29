a626d6559b477b1e7c08913605a2819c0c021fb9
NeilwBailey <neilwbailey@gmail.com>
2021-12-29 21:59:56 +1100

    Added more license info

--- fastica_nwb.m
+++ fastica_nwb.m
@@ -5,6 +5,19 @@ function [Out1, Out2, Out3, NonConvergence] = fastica_nwb(mixedsig, varargin) % 
 % Version 2.5, October 19 2005
 % Copyright (c) Hugo Gävert, Jarmo Hurri, Jaakko Särelä, and Aapo Hyvärinen.
 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see https://www.gnu.org/licenses/.
+
 %% NWB added output for files showing non-convergence, addition noted in the code at line 476
 
 % FASTICA(mixedsig) estimates the independent components from given
--- fpica_nwb.m
+++ fpica_nwb.m
@@ -1,3 +1,18 @@
+% Copyright (c) Hugo Gävert, Jarmo Hurri, Jaakko Särelä, and Aapo Hyvärinen.
+
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see https://www.gnu.org/licenses/.
+
 function [A, W, NonConvergence] = fpica_nwb(X, whiteningMatrix, dewhiteningMatrix, approach, ...
 			numOfIC, g, finetune, a1, a2, myy, stabilization, ...
 			epsilon, maxNumIterations, maxFinetune, initState, ...
